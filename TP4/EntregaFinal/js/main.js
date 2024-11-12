f22b7b78f89cc61dace5adf6571ce7b186406a8b
Luka Saldivia <saldivialuka@gmail.com>
2024-11-12 19:45:46 -0300

    download section

---
+++ TP4/EntregaFinal/js/main.js
@@ -0,0 +1,19 @@
+let $ = (selector) => document.querySelector(selector)
+let $$ = (selector) => document.querySelectorAll(selector)
+
+// Sección "Descubre el juego que convierte las Matemáticas en diversión"
+let download_section = $('section.download')
+
+download_section.addEventListener('mousemove', ({clientX, clientY}) => {
+
+  let {height, width} = download_section.getBoundingClientRect()
+
+  let x = clientX / width - .5  
+  let y = clientY / height - .5
+  
+  download_section.setAttribute('style', `--_mouse-x : ${x}; --_mouse-y : ${y}`)
+  
+  
+})
+
+
