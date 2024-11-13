fddae66861c3195a157dd365a56cb66913b06a7f
Luka Saldivia <saldivialuka@gmail.com>
2024-11-12 22:22:46 -0300

    cols appear

--- TP4/EntregaFinal/js/main.js
+++ TP4/EntregaFinal/js/main.js
@@ -1,8 +1,21 @@
 let $ = (selector) => document.querySelector(selector)
 let $$ = (selector) => document.querySelectorAll(selector)
 
+// Sección "La app más divertida y educativa y para niños de 3 años"
+const columns = $$('.cols-3 > .col');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    columns.forEach(el => el.classList.toggle('visible', entry.isIntersecting))
+  });
+}, { threshold: 0 });
+
+columns.forEach(el => observer.observe(el))
+
+
+
 // Sección "Descubre el juego que convierte las Matemáticas en diversión"
-let download_section = $('section.download')
+const download_section = $('section.download')
 
 download_section.addEventListener('mousemove', ({clientX, clientY}) => {
   let {height, width} = download_section.getBoundingClientRect()
